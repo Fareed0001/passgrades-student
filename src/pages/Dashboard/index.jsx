81fad20cb1eeae507dcc0691111afdf256dd21ed
Farid <fareedibrahim0003@gmail.com>
2023-07-30 18:38:36 +0100

    mobile nav

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import React, { useEffect } from "react";
 import Router from "next/router";
-// import Cookies from "js-cookie";
+import Cookies from "js-cookie";
 
 const Dashboard = () => {
   const { status, data } = useSession();
