32aa7ec12900466bd901ba59ce5fe1cc795f1443
Farid <fareedibrahim0003@gmail.com>
2023-08-16 07:20:39 +0100

    redirect link removed

--- src/pages/api/auth/redirect.js
+++
@@ -1,8 +0,0 @@
-// pages/api/redirect.js
-
-export default function handler(req, res) {
-    // Perform a 301 permanent redirect to the new URL
-    res.writeHead(301, { Location: 'https://passgrades.com' });
-    res.end();
-  }
-  
